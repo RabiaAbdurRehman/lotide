3cdeee62a2d2680250a40d0e4ed72107472423a9
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-05-19 19:38:24 -0600

    making a new array who has all elements of original array except first one

---
+++ tail.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+    if(actual !== expected){
+        console.log(`Assertion Passed: ${actual} === ${expected}\u{1F614}`);
+    }
+
+    else {
+      console.log(`Assertion Passed: ${actual} === ${expected}\u{1F60A}`);
+
+
+    }
+
+  };
+  const tail = function(arr){
+    let arrayNew = " ";
+    aarayNew = arr.slice(1);
+
+       return arrayNew;
+
+};
+
+  //assertEqual(1,1);
+  //assertEqual('Lighthouse', 'Bootcamp');
+  // Test Case: Check the original array
+const arr = ["Yo Yo", "Lighthouse", "Labs"];
+const words = [];
+const news = [1];
+const num = [1 , 2 , 3, 4];
+tail(arr); // no need to capture the return value since we are not checking it
+tail(words);
+tail(news);
+tail(num);
+assertEqual(arr.length, 3); // original array should still have 3 elements!
+assertEqual(words.length, 0);
+assertEqual(news.length, 0);
+assertEqual(num.length, 3);
