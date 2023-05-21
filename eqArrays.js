ba41dda7953dc98efc376e8a10fde95fac6c5f34
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-05-21 13:30:38 -0600

    I made some changes with eslint

--- eqArrays.js
+++ eqArrays.js
@@ -1,36 +1,22 @@
-
-const assertEqual = function(actual, expected) {
-    if(actual === expected){
-        console.log("True");
-
-    } else{
-        console.log("Fail");
-    }
-
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
-    const eqArrays = function(arr1 , arr2){
-        if (arr1.length !== arr2.length) {
-            return false;
-        }
-        for(let i = 0; i<arr1.length; i++){
-
-
-                if(arr1[i] !== arr2[i]){
-                   return false;
-
-
-                }
-
-}
-        return true;
-
-
-   };
-
-
-
-  //const eqArrays([1, 2, 3], [1, 2, 3]) // => true
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-  assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+const eqArrays = function(arr1 , arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
