9bb6afac19734461cdbaffb536e967006bebf903
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-05-25 03:41:21 -0600

    Assignment comparing objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,57 @@
+const eqArrays = function(arr1 , arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let key in object1) {
+    const typeOfValue = typeof object1[key]; //good strategy to find type.
+    console.log("type of value ", typeOfValue);
+    if (typeOfValue === "object") {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+
+  }
+
+  return true;
+};
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; // <= add this line built in function inspect.
+
+  if (eqObjects(actual, expected)) {
+    console.log(`Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    //console.log(`Assertion Passed: ${actual} === ${expected}`);
+    //console.log(`Passed: `, [actual, expected]);
+  } else {
+    console.log(`Assertion failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    //console.log(`Assertion failed: ${actual} !== ${expected}`)
+
+  }
+};
+
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject);
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertObjectsEqual(multiColorShirtObject, longSleeveMultiColorShirtObject);
+
