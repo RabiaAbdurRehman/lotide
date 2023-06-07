41c90852cb03c29c743c504627ba1a473285007d
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-07 07:19:01 -0600

    modified using export/import

--- head.js
+++ head.js
@@ -1,21 +1,9 @@
-const assertEqual = function(actual, expected) {
-  console.assert(actual === expected, `${actual} !== ${expected}\u{1F614}`);
-
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}\u{1F60A}`);
-
-
-  }
-
-};
+const assertEqual = require('./assertEqual');
 const head = function(array) {
   let firstElement = array[0];
   return firstElement;
 
 };
 
-assertEqual(1,1);
-assertEqual('Lighthouse', 'Bootcamp');
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+module.exports= head;
+
