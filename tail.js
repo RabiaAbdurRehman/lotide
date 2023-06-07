c883d63ae48614943613a5caeab2c31014584278
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-07 11:04:51 -0600

    Refactoring

--- tail.js
+++ tail.js
@@ -1,35 +1,22 @@
-const assertEqual = function(actual, expected) {
-    if(actual !== expected){
-        console.log(`Assertion Passed: ${actual} === ${expected}\u{1F614}`);
-    }
+const assertEqual = require('./assertEqual');
+const tail = function(arr) {
+  let arrayNew = " ";
+  arrayNew = arr.slice(1);
 
-    else {
-      console.log(`Assertion Passed: ${actual} === ${expected}\u{1F60A}`);
-
-
-    }
-
-  };
-  const tail = function(arr){
-    let arrayNew = " ";
-    arrayNew = arr.slice(1);
-
-       return arrayNew;
+  return arrayNew;
 
 };
-
-  //assertEqual(1,1);
-  //assertEqual('Lighthouse', 'Bootcamp');
-  // Test Case: Check the original array
-const arr = ["Yo Yo", "Lighthouse", "Labs"];
-const words = [];
-const news = [1];
-const num = [1 , 2 , 3, 4];
-tail(arr); // no need to capture the return value since we are not checking it
-tail(words);
-tail(news);
-tail(num);
-assertEqual(arr.length, 3); // original array should still have 3 elements!
-assertEqual(words.length, 0);
-assertEqual(news.length, 0);
-assertEqual(num.length, 3);
+// const arr = ["Yo Yo", "Lighthouse", "Labs"];
+// const words = [];
+// const news = [1];
+// const num = [1, 2, 3, 4];
+// console.log(tail(arr)); // no need to capture the return value since we are not checking it
+// tail(words);
+// tail(news);
+// tail(num);
+// assertEqual(tail(arr).length, 2);
+// assertEqual(arr.length, 3); // original array should still have 3 elements!
+// assertEqual(words.length, 0);
+// assertEqual(news.length, 0);
+// assertEqual(num.length, 3);
+module.exports = tail;
