b8ca3e0905f4e67eb241b71f3b2dfe3d2da23fe8
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-07 12:48:56 -0600

    modification of middle.js

--- middle.js
+++ middle.js
@@ -1,36 +1,20 @@
-const eqArrays = function(arr1 , arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log("The middle element(s) of the array is/are: ", expected);
-
-  } else {
-    console.log("The middle element(s) of the array is/are not confirmed.");
-  }
-
-};
-
-
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 const middle = function(array) {
-  let middle = Math.floor(array.length / 2);
-  if (array.length % 2 === 0) {
-    return [array[middle - 1], array[middle]];
+  let middle = Math.floor(array.length / 2); //case 1: 5/2 = 2.5, then math.floor(2.5) = 2, case 2: 6/2 = 3
+  if (array.length % 2 === 0) {               // case 2
+    return [array[middle - 1], array[middle]]; // case 2: array[2], array[3]
   } else {
-    return [array[middle]];
+    return [array[middle]];// case 1: array[2] = 3
   }
 
 };
+module.exports = middle;
 
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [1, 2, 3, 4, 5, 6];
+console.log("The middle elements of the array is/are: ", middle(arr1));
+console.log("The middle elements of the array is/are: ", middle(arr2));
 
-
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+assertArraysEqual(middle(arr1), [3]); // case 1
+assertArraysEqual(middle(arr2), [3, 4]);  //case 2, => [3, 4]
